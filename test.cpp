d22635656c3270977eadb87260a4299957035a16
ParfenovSergey <Parfenov.Sergey.mstu@mail.ru>
2026-03-12 14:05:26 +0300

    Added first SDL test

---
+++ test.cpp
@@ -0,0 +1,69 @@
+#include "test.hpp"
+
+#include <iostream>
+int main(int argv, char** argc) {
+  Game g;
+  if (g.init("TEST WINDOW", 640, 480,
+             SDL_WINDOW_RESIZABLE | SDL_WINDOW_MINIMIZED)) {
+    g.startGame();
+  } else {
+    return 1;
+  }
+
+  while (g.isRunning()) {
+    g.handleEvents();
+    g.update();
+    g.render();
+  }
+  g.clean();
+  return 0;
+}
+
+bool Game::init(std::string title, int w, int h, int flags) {
+  window_ = SDL_CreateWindow(title.c_str(), w, h, flags);
+  if (window_ != 0) {
+    std::cout << "window created" << std::endl;
+    renderer_ = SDL_CreateRenderer(window_, NULL);
+    if (renderer_ != 0) {
+      std::cout << "renderer created" << std::endl;
+      SDL_SetRenderDrawColor(renderer_, 255, 0, 0, 255);
+    } else {
+      std::cerr << "renderer error" << std::endl;
+      return false;
+    }
+  } else {
+    std::cerr << "window error" << std::endl;
+    return false;
+  }
+  return true;
+}
+
+void Game::render() {
+  SDL_RenderClear(renderer_);
+  SDL_RenderPresent(renderer_);
+}
+
+void Game::update() {}
+
+void Game::handleEvents() {
+  SDL_Event event;
+  if (SDL_PollEvent(&event)) {
+    switch (event.type) {
+      case SDL_EVENT_QUIT:
+        stopGame();
+        break;
+      case SDL_EVENT_WINDOW_RESIZED:
+        std::cout << "resize" << std::endl;
+        break;
+      default:
+        break;
+    }
+  }
+}
+
+void Game::clean() {
+  std::cout << "exit" << std::endl;
+  SDL_DestroyRenderer(renderer_);
+  SDL_DestroyWindow(window_);
+  SDL_Quit();
+}
---
+++ test.hpp
@@ -0,0 +1,26 @@
+#include <SDL3/SDL.h>
+#include <SDL3/SDL_main.h>
+
+#include <string>
+
+SDL_Window* g_pWindow = nullptr;
+SDL_Renderer* g_pRenderer = nullptr;
+
+class Game {
+ public:
+  Game() {}
+  ~Game() {}
+  bool init(std::string, int, int, int);
+  void render();
+  void update();
+  void handleEvents();
+  void clean();
+  void startGame() { running_ = true; }
+  void stopGame() { running_ = false; }
+  bool isRunning() { return running_; }
+
+ private:
+  bool running_ = false;
+  SDL_Window* window_ = nullptr;
+  SDL_Renderer* renderer_ = nullptr;
+};
